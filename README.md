cdc9864b1cac721f0ab350e6d25da5741e523036
Annemarie Allen <annemarie@Annemaries-MacBook-Pro.local>
2020-04-22 18:03:43 -0800

    completed Tabs/index.js instructions

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ A component is a part or element, a resuable piece, that we ccna layer on top of
 
 Your finished project must include all of the following requirements:
 
-* [ ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the HTML file itself.
+* [ ] Look through the HTML code and familiarize yourself with the different sections. Some of them already exist, but some need to be filled in. DO NOT add any code to the HTMLnpm file itself.
 
 * [ ] Following the instructions in the `Header/index.js` file, create the Header component.
 
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,3 +1,5 @@
+
+
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
@@ -7,3 +9,24 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+
+console.log(axios.get('https://lambda-times-backend.herokuapp.com/topics'));
+
+
+const tab = document.querySelector(".topics");
+
+function tabsContent(object){
+    object.topics.map(el => {
+    const tabs = document.createElement("div");
+    tabs.classList.add('tab');
+    tab.appendChild(tabs)
+    })
+      
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => tabsContent(response.data));
+
